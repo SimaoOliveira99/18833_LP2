4176205b88c3f9519f7787d4077256f3ef920b32
Simao Oliveira <99simaooliveira@gmail.com>
2020-04-25 16:07:08 +0100

    adiciona ocorrencias

---
+++ GestorOcorrencias/Ocorrencia.cs
@@ -0,0 +1,98 @@
+﻿using System;
+namespace GestorOcorrencias
+{
+    public class Ocorrencia
+    {
+        #region ESTADO
+
+        static int id;
+        static DateTime data;
+        static string descricao;
+        static int idCatastrofe;
+        static int idDistrito;
+
+        #endregion
+
+        #region METODOS
+
+        #region CONSTRUTORES
+
+        public Ocorrencia(int idOc, DateTime dt, int idCat, int idDis)
+        {
+            id = idOc;
+            data = dt;
+            //verificar se existe o idCat e idDis na lista de catastrofes e de distritos respetivamente
+            idCatastrofe = idCat;
+            idDistrito = idDis;
+        }
+
+        public Ocorrencia(int idOc, DateTime dt, string desc, int idCat, int idDis)
+        {
+            id = idOc;
+            data = dt;
+            descricao = desc;
+            //verificar se existe o idCat e idDis na lista de catastrofes e de distritos respetivamente
+            idCatastrofe = idCat;
+            idDistrito = idDis;
+        }
+
+        #endregion
+
+        #region PROPRIEDADES
+
+        public int Id
+        {
+            get { return id; }
+        }
+
+        public DateTime Data
+        {
+            get { return data; }
+            set
+            {
+                DateTime aux;
+                if (DateTime.TryParse(value.ToString(), out aux) == true)
+                    data = value;
+            }
+        }
+
+        public string Descricao
+        {
+            get { return descricao; }
+            set { descricao = value; }
+        }
+
+        public int IdCatastrofe
+        {
+            get { return idCatastrofe; }
+            //verificar se existe o id vindo do value na lista de catastrofes
+            set { idCatastrofe = value; }
+        }
+
+        public int IdDistrito
+        {
+            get { return idDistrito; }
+            //verificar se existe o id vindo do value na lista de distritos
+            set { idDistrito = value; }
+        }
+
+        #endregion
+
+        #region METODOS_DE_CLASSE
+
+        /// <summary>
+        /// Compara id recebido com id da ocorrencia.
+        /// </summary>
+        /// <returns><c>true</c>, if identifier was equalsed, <c>false</c> otherwise.</returns>
+        /// <param name="idToCompare">Identifier to compare.</param>
+        public bool EqualsId(int idToCompare)
+        {
+            return id == idToCompare;
+        }
+
+        #endregion
+
+        #endregion
+
+    }
+}
---
+++ GestorOcorrencias/Ocorrencias.cs
@@ -0,0 +1,80 @@
+﻿using System;
+namespace GestorOcorrencias
+{
+    public class Ocorrencias
+    {
+        static int totalOcorrencias;
+        static Ocorrencia[] ocorrencias;
+        const int MAX = 10;
+
+        public Ocorrencias()
+        {
+            totalOcorrencias = 0;
+            ocorrencias = new Ocorrencia[MAX];
+        }
+
+        #region METODOS
+
+        #region INDEXADORES
+
+        /*public Ocorrencias this[int i]
+        {
+            get { return ocorrencias[i]; }
+            //FIXME validar se a posição já n está ocupada, se nao estiver alterar senao nao alterar
+            set { ocorrencias[i] = new Ocorrencia(value); totalOcorrencias++; }     //Validações
+        }*/
+
+        #endregion
+
+
+        public static int InsereOcorrencia(DateTime data, int idCatastrofe, int idDistrito)
+        {
+            if (totalOcorrencias < MAX)
+            {
+                totalOcorrencias++;
+                Ocorrencia newOcorr = new Ocorrencia(totalOcorrencias, data, idCatastrofe, idDistrito);
+                ocorrencias[totalOcorrencias] = newOcorr;
+                return totalOcorrencias;
+            }
+            return 0;
+        }
+
+        public static int InsereOcorrencia(DateTime data, string descricao, int idCatastrofe, int idDistrito)
+        {
+
+            if (totalOcorrencias < MAX)
+            {
+                //Ocorrencia newOcorr = new Ocorrencia(totalOcorrencias++, data, descricao, idCatastrofe, idDistrito);
+                ocorrencias[totalOcorrencias] = new Ocorrencia(totalOcorrencias + 1, data, descricao, idCatastrofe, idDistrito);
+                totalOcorrencias++;
+                return totalOcorrencias;
+            }
+
+            return 0;
+        }
+
+        /// <summary>
+        /// Procura ocorrencia através do Id.
+        /// </summary>
+        /// <returns>Se encontrar retorna a ocorrência senão retorna null.</returns>
+        /// <param name="id">Identifier.</param>
+        public static Ocorrencia ProcuraOcorrencia(int id)
+        {
+            for (int i = 0; i < totalOcorrencias; i++)
+            {
+                if (ocorrencias[i].EqualsId(id))
+                {
+                    return ocorrencias[i];
+                }
+            }
+
+            return null;
+        }
+
+        //func adicionar dano
+        //func adicionar obito
+        //func adicionar ferido
+
+        #endregion
+    }
+}
